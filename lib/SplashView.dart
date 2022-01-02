356234d696a72d13c18c22ad0fcfec946675f16d
人生如梦 <sino_miles@foxmail.com>
2022-01-02 17:34:04 +0800

    Update SplashView.dart

--- lib/SplashView.dart
+++ lib/SplashView.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:ks_ads_flutter/ks_ads_callback.dart';
 
 class SplashAdView extends StatefulWidget {
   final String androidCodeId;
+  final String? iosidCodeId;
   final SplashViewCallback? callBack;
-  SplashAdView({Key? key, required this.androidCodeId, this.callBack})
+  SplashAdView(
+      {Key? key, required this.androidCodeId, this.callBack, this.iosidCodeId})
       : super(key: key);
 
   @override
@@ -17,16 +20,29 @@ class _SplashAdViewState extends State<SplashAdView> {
   MethodChannel? _channel;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: AndroidView(
-        viewType: _viewType,
-        creationParams: {"androidCodeId": widget.androidCodeId},
-        onPlatformViewCreated: _registerChannel,
-        creationParamsCodec: const StandardMessageCodec(),
-      ),
-    );
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: AndroidView(
+          viewType: _viewType,
+          creationParams: {"androidCodeId": widget.androidCodeId},
+          onPlatformViewCreated: _registerChannel,
+          creationParamsCodec: const StandardMessageCodec(),
+        ),
+      );
+    } else {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: UiKitView(
+          viewType: _viewType,
+          creationParams: {"iosCodeId": widget.iosidCodeId},
+          onPlatformViewCreated: _registerChannel,
+          creationParamsCodec: const StandardMessageCodec(),
+        ),
+      );
+    }
   }
 
   //注册channel
@@ -38,7 +54,10 @@ class _SplashAdViewState extends State<SplashAdView> {
   //监听原生view传值
   Future<dynamic> _platformCallHandler(MethodCall call) async {
     switch (call.method) {
-      case "onSkippedAd":
+      case "onShow":
+        widget.callBack!.onShow!();
+        break;
+      case "onSkip":
         widget.callBack!.onSkip!();
         break;
       case "onAdShowEnd":
