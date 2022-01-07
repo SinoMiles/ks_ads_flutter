c86fed05cdd115ef4e7128bf74fba454cac7e34c
人生如梦 <sino_miles@foxmail.com>
2022-01-07 15:54:14 +0800

    Update KsAdsFlutterPlugin.m

--- ios/Classes/KsAdsFlutterPlugin.m
+++ ios/Classes/KsAdsFlutterPlugin.m
@@ -1,91 +1,51 @@
-#import "SplashViewFactory.h"
+#import "KsAdsFlutterPlugin.h"
 #import <KSAdSDK/KSAdSDK.h>
+#import "RewardVideoViewFactory.h"
+#import "KsRewardVideo.h"
+#import "KsFlutterEvent.h"
 
+KsFlutterEvent *ad_event;
 
-
-@interface SplashView () <KSSplashAdViewDelegate>
-
-@end
-
-@implementation SplashViewFactory {
-  NSObject<FlutterBinaryMessenger>* _messenger;
+@implementation KsAdsFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"ks_ads_flutter"
+            binaryMessenger:[registrar messenger]];
+  KsAdsFlutterPlugin* instance = [[KsAdsFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+  [registrar registerViewFactory:[[RewardVideoViewFactory alloc] initWithMessenger:registrar.messenger] withId:@"com.ahd.ks_ads.reward_video"];
+  ad_event = [[KsFlutterEvent alloc] initWithRegistrar:registrar];
 }
 
-- (instancetype)initWithMessenger:(NSObject<FlutterBinaryMessenger>*)messenger {
-  self = [super init];
-  if (self) {
-    _messenger = messenger;
-  }
-  return self;
-}
-
-- (nonnull NSObject<FlutterPlatformView> *)createWithFrame:(CGRect)frame viewIdentifier:(int64_t)viewId arguments:(id _Nullable)args {
-    SplashView *videoView = [[SplashView alloc] initWithFrame:frame viewIdentifier:viewId arguments:args binaryMessenger:_messenger];
-    return videoView;
-}
-@end
-
-@implementation SplashView {
-    UIWindow *_container;
-    KSSplashAdView *splashAdView;
-    FlutterMethodChannel* _channel;
-}
-
-
-- (instancetype)initWithFrame:(CGRect)frame
-               viewIdentifier:(int64_t)viewId
-                    arguments:(id _Nullable)args
-              binaryMessenger:(NSObject<FlutterBinaryMessenger>*)messenger {
-    NSString *methodName = [NSString stringWithFormat:@"com.miles.ksAd/SplashAdView_%lld", viewId];
-    _channel = [FlutterMethodChannel methodChannelWithName:methodName binaryMessenger:messenger];
-    _container = [[UIWindow alloc] initWithFrame:frame];
-    splashAdView= [[KSSplashAdView alloc] initWithPosId:@"8705000016"];
-    splashAdView.delegate=self;
-    splashAdView.rootViewController= [[UIApplication sharedApplication] keyWindow].rootViewController;;
-    [splashAdView loadAdData];
-  return self;
-}
-
- 
-#pragma -mark KSSplashAdDelegate
-- (void)ksad_splashAdDidLoad:(KSSplashAdView *)splashAdView {
-
-}
-- (void)ksad_splashAdContentDidLoad:(KSSplashAdView *)splashAdView {
-     [splashAdView showInView:[[UIApplication sharedApplication] keyWindow]];
-}
-- (void)ksad_splashAdDidVisible:(KSSplashAdView *)splashAdView {
-    [_channel invokeMethod:@"onShow" arguments:nil];
-}
-- (void)ksad_splashAdVideoDidBeginPlay:(KSSplashAdView *)splashAdView {
- 
-}
-- (void)ksad_splashAd:(KSSplashAdView *)splashAdView didFailWithError:(nonnull NSError
-*)error {
-    [_channel invokeMethod:@"onError" arguments:[NSString stringWithFormat:@"错误码：%ld", (long)error.code]];
-}
-- (void)ksad_splashAd:(KSSplashAdView *)splashAdView didSkip:
-(NSTimeInterval)playDuration {
-    [_channel invokeMethod:@"onSkip" arguments:nil];
-    [splashAdView removeFromSuperview];
-    splashAdView=nil;
-}
-
-- (void)ksad_splashAd:(KSSplashAdView *)splashAdView didClick:(BOOL)onZoomOutState {
- //点击跳转了
- 
-}
-- (void)ksad_splashAdDidAutoDismiss:(KSSplashAdView *)splashAdView {
-
-}
-- (void)ksad_splashAdDidClose:(KSSplashAdView *)splashAdView {
-    [_channel invokeMethod:@"onClose" arguments:nil];
-    [splashAdView removeFromSuperview];
-    splashAdView=nil;
-}
-
-- (UIView*)view {
-  return _container;
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    }
+    else if ([@"getSdkVersion" isEqualToString:call.method]) {
+        result(KSAdSDKManager.SDKVersion);
+    }
+    else if ([@"register" isEqualToString:call.method]) {
+        NSDictionary *map = call.arguments;
+        NSString *appId = [map valueForKey:@"appId"];
+        [KSAdSDKManager setAppId:appId];
+        [KSAdSDKManager setLoglevel:KSAdSDKLogLevelOff];
+        result([NSNumber numberWithBool:true]);
+    }
+    else if ([@"loadAndShowRewardVideo" isEqualToString:call.method]) {
+        [[KsRewardVideo instance] loadAndShowRewardVideo:call.arguments];
+        result(nil);
+    }
+    else if ([@"loadRewardVideo" isEqualToString:call.method]) {
+        [[KsRewardVideo instance] loadRewardVideoWithArgs:call.arguments];
+        result(nil);
+    }
+    else if ([@"showRewardVideo" isEqualToString:call.method]) {
+        [[KsRewardVideo instance] showRewardVideo];
+        result(nil);
+    }
+    else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
